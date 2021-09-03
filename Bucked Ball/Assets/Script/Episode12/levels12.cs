8ebbb796709eb271ed8539c04c432994fcc77051
Kenan <kenan.binay@hotmail.com>
2021-09-03 23:04:38 +0300

    BuckedBall 03.09.2021
    Episode 11 done epis 12 kinda done

--- Bucked Ball/Assets/Script/Episode12/levels12.cs
+++ Bucked Ball/Assets/Script/Episode12/levels12.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class levels12 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GetLevels1()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Time.timeScale = 1f;
+        Bucked11To12.x12 = 1;
+        SceneManager.LoadScene(1);
     }
 }
