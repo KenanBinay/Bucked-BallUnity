5894f1e4a43a9d10128b57479de08275d7e3a61a
Kenan <kenan.binay@hotmail.com>
2021-09-27 03:20:56 +0300

    GearDoorsAdded

--- Bucked Ball/Assets/Script/Episode11/Key11.cs
+++ Bucked Ball/Assets/Script/Episode11/Key11.cs
@@ -7,12 +7,14 @@ public class Key11 : MonoBehaviour
     public Animator Diken1;
     public Animator Diken2;
     public Animator Diken3;
+    public Animator Diken4;
 
     private void Start()
     {
         Diken1.enabled = false;
         Diken2.enabled = false;
         Diken3.enabled = false;
+        Diken4.enabled = false;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -23,6 +25,7 @@ public class Key11 : MonoBehaviour
             Diken1.enabled = true;
             Diken2.enabled = true;
             Diken3.enabled = true;
+            Diken4.enabled = false;
         }
 
     }
