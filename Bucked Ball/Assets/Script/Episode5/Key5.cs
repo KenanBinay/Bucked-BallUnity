da4c8c422b93a8b7bea03d08c8847f306686ac67
Kenan <kenan.binay@hotmail.com>
2021-10-11 04:23:20 +0300

    KeySfxAdded

--- Bucked Ball/Assets/Script/Episode5/Key5.cs
+++ Bucked Ball/Assets/Script/Episode5/Key5.cs
@@ -7,22 +7,26 @@ public class Key5 : MonoBehaviour
     public Animator Diken1;
     public Animator Diken2;
     public Animator Diken3;
-
+    public Animator Diken4;
+    public AudioSource KeySource;
     private void Start()
     {
         Diken1.enabled = false;
         Diken2.enabled = false;
         Diken3.enabled = false;
+        Diken4.enabled = false;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag=="Player")
         {
+            KeySource.Play();
             Destroy(gameObject);
             Diken1.enabled = true;
             Diken2.enabled = true;
             Diken3.enabled = true;
+            Diken4.enabled = true;
         }
        
     }
